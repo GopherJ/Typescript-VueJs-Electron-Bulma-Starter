6c7e854a0393e12e2244e975ab5e69c323de95c3
Cheng JIANG <cheng.jiang@outlook.fr>
2018-06-27 11:32:54 +0200

    websocket ok

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,38 +3,27 @@ import * as bodyParser from 'body-parser'
 import * as WebSocket from 'ws'
 import * as http from 'http'
 import * as url from 'url'
-import * as config from '../config/index.json'
+import * as path from 'path'
 
 // Create Express server
 const app = express()
+
+// Port
+const port = 1919
 
 // Initialize a simple http server
 const server = http.createServer(app)
 
 // Initialize the WebSocket server instance
-const wss = new WebSocket.Server({ noServer: true })
-
-wss.on('connection', function(ws) {
-  console.log(ws)
+const wss = new WebSocket.Server({ server, port: 40080 })
+wss.on('connection', function connection(ws) {
+  ws.send(JSON.stringify({a: 1}))
 })
-
-server.on('upgrade', function upgrade(request, socket, head) {
-  const pathname = url.parse(request.url).pathname;
-  if (pathname !== '/ws') {
-    socket.destroy();
-  }
-
-  wss.handleUpgrade(request, socket, head, function done(ws) {
-    wss.emit('connection', ws, request);
-  });
-})
-
 // Express configuration
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+
+app.use(express.static(path.join(__dirname, '../client/index.html')))
 
 // Run server
-app.listen((<any>config).server.port, () => console.log(`Server is listening on port ${(<any>config).server.port}!`))
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
